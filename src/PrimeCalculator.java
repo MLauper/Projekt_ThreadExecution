a38508382a417d8bc0143842d307c7b34270a3cb
Marco Lauper <mauskatzehund@gmail.com>
2014-05-20 22:56:10 +0200

    Step

--- src/PrimeCalculator.java
+++ src/PrimeCalculator.java
@@ -3,24 +3,29 @@
  */
 public class PrimeCalculator extends Thread {
     String name;
+    int curNum;
 
     public PrimeCalculator(String name) {
         this.name = name;
+        this.start();
     }
 
     @Override
     public void run() {
-        super.run();
-    }
-
-    @Override
-    public synchronized void start() {
-        super.start();
+        log("Started Execution");
+        curNum = 1000000000;
+        for (;!(this.isInterrupted());curNum++){
+            if(isPrime(curNum)) log("PRIME: " + curNum);
+        }
     }
 
     public static boolean isPrime(int no){
         if (no < 2) return false;
         for (int i = 2; i < no; i++){ if (no % i == 0) return false; }
         return true;
+    }
+
+    private void log(String message){
+        System.out.println("Thread " + name + ": " + message);
     }
 }
--- src/Version1_main.java
+++ src/Version1_main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Marco on 20.05.2014.
@@ -9,5 +12,16 @@ public class Version1_main {
         // TODO: When the calculation of prime numbers (2, 3, …, m) is completed in the main method, interrupt all the other running threads.
         // TODO: When all threads (except the main one) have been terminated, display for each thread the highest prime number that was calculated.
 
+        int numOfThreads = 1;
+        List<Thread> workers = new ArrayList<Thread>(numOfThreads);
+        for (int num = 1; num <= numOfThreads; num++){
+            workers.add(new PrimeCalculator("Worker " + num));
+        }
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("finisch...");
+        for(Thread thread : workers){
+            thread.interrupt();
+        }
     }
 }
